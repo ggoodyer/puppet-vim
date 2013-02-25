57f319a35a2271f72981de92c71769ed9c50f4d4
George Goodyer <george.goodyer@rightscale.com>
2013-02-25 14:39:54 -0800

    sync

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,13 +6,13 @@
 
 class vim {
  
-  exec { "git clone https://github.com/raphael/adam-vim.git /Users/ggoodyer/.vim":
+  exec { 'git clone https://github.com/raphael/adam-vim.git /Users/ggoodyer/.vim':
     path => "/opt/boxen/homebrew/bin/",
     creates => "/Users/ggoodyer/.vim",
     logoutput => "true",
   }
 
-  exec { "ln -s /Users/ggoodyer/.vim/.vimrc /Users/ggoodyer/.vimrc":
+  exec { 'ln -s /Users/ggoodyer/.vim/.vimrc /Users/ggoodyer/.vimrc':
     path => "/bin/",
     creates => "/Users/ggoodyer/.vimrc",
     logoutput => "true",
