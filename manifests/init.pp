af31ef85a6c8c7bea4eb2b2b4d6c07727a61f444
George Goodyer <george.goodyer@rightscale.com>
2013-02-27 09:41:30 -0800

    debug

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,14 +14,17 @@ class adamvim {
     ensure => directory
   }
 
-  notify{"The  data value is: ${adamvim::config::bin}": }
+  notify{"The bin value is: ${adamvim::config::bin}": }
+  notify{"The vimdest value is: ${adamvim::config::vimdest}": }
+  notify{"The data value is: ${adamvim::config::boxendatadir}": }
+  notify{"The uri value is: ${adamvim::config::uri}": }
 
   # clone the git repo to tmp
   exec { 'clone adam-vim.git':
     command   => "git clone ${adamvim::config::uri}",
     path      => "${adamvim::config::bin}",
     creates   => "${adamvim::config::vimdest}",
-    cwd       => "${adamvim::config::datadir}",
+    cwd       => "${adamvim::config::boxendatadir}",
     tries     => "3",
     timeout   => "15",
     logoutput => "true",
